c52bf7b6be21d17e1525a650d5c63bc91d00425e
Dishb27 <dbawanga27@gmail.com>
2025-11-06 09:35:11 +0530

    commit changes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,9 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 import "@/styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 App.propTypes = {
